a7619f6cbb60d4efe2d800dd939b490bf39203a8
Xcamaj <Xhemajlcamaj@gmail.com>
2022-03-13 14:18:41 -0400

    added prompts

--- Develop/app.js
+++ Develop/app.js
@@ -10,6 +10,24 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const employeeQuestions = ([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Enter employee name.'
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Enter employee id.'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter employee email.'
+    },
+])
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
